bdc0d74e4d1a0b6dcafb67e8aef219ad53cb141f
vagrant <vagrant@homestead>
2021-03-06 08:11:12 +0000

    基础页面

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Weibo App</title>
+    <title>@yield('title', 'Weibo App') -- laravel 初级教程</title>
 </head>
 <body>
     @yield('content')
